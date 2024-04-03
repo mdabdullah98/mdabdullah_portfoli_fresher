157b0e4fc74305d6622c7f6163ce780c151092f2
md abdullah <abdulhussain78615@gmail.com>
2024-04-03 23:32:02 +0530

    intial commit

---
+++ src/Education_Experience.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+const Experience = (
+  <div>
+    <div>
+      <h4>Objective</h4>
+      <p>
+        Hardworking and passionate job seeker with strong organizational skills
+        eager to secure entry-level Frontend developer position. Ready to help
+        team achieve company goals.
+      </p>
+    </div>
+    <h4 className="my-3">Work Experience</h4>
+    <h5>Concentrix Daksh services India Pvt Ltd.</h5>
+    <div className="d-flex justify-content-between">
+      <p>Content Moderation</p>
+    </div>
+    <ul>
+      <li>
+        Understanding client policies and guidelines for content creation and
+        moderation.
+      </li>
+
+      <li>
+        Conducting brand-building activities and enhancing the online reputation
+        of the platform.
+      </li>
+      <li>
+        Identifying areas of improvement and suggesting solutions for content
+        performance and user engagement.
+      </li>
+      <li>
+        Reviewing and moderating various types of user-generated content, such
+        as text, images, video, and audio. Removing inappro
+      </li>
+    </ul>
+  </div>
+);
+
+const Education = (
+  <div>
+    <h3 className="my-5">Education</h3>
+    <h6 className="h6 text-danger mb-1">2017</h6>
+    <p className="mb-1">College Degree ,University of Delhi</p>
+    <h6 className="text-danger mb-1">2014</h6>
+    <p className="mb-1">Senior Secondary Education (12Th passed) ,CBSE</p>
+    <h6 className="text-danger mb-1">2012</h6>
+    <p className="mb-1">Secondary Education (10Th passed) ,CBSE</p>
+  </div>
+);
+export { Experience, Education };
